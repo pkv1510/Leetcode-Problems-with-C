825691d8e8dd293642c9b4ec1eee2a28c5387e25
Prabhav <pvkumar5749404@gmail.com>
2023-12-16 17:25:13 +0530

    [ Problem 2 ] : Solution for Leetcode problem using C with different methods
    Signed-off-by: Prabhav <pvkumar5749404@gmail.com>

---
+++ addNum.c
@@ -0,0 +1,134 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct ListNode {
+    int val;
+    struct ListNode* next;
+};
+
+void printList(struct ListNode* ptr) {
+    while(ptr != NULL) {
+        printf("%d ", ptr->val);
+        ptr = ptr->next;
+    }
+    printf("\n");
+}
+
+//Method-1
+struct ListNode* createNode(struct ListNode** head, int value) {
+    struct ListNode* temp = NULL;
+    temp = (struct ListNode*)malloc(sizeof(struct ListNode));
+    temp->next = NULL;
+    temp->val = value; 
+
+    if(*head == NULL)
+        *head = temp;
+
+    return temp;
+}
+
+void addNode(struct ListNode* head, struct ListNode* temp) {
+    struct ListNode* ptr = head;
+    if(head == temp)
+        return;
+    while(ptr->next != NULL) {
+        ptr = ptr->next;
+    }
+    ptr->next = temp;
+}
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    int sum, carryFlag = 0;
+    struct ListNode* head = NULL;
+
+    if(l1 == NULL && l2 == NULL)
+        return NULL;
+
+    while(l1 != NULL && l2 != NULL) {
+	    if(l1 != NULL && l2 != NULL) {
+	        sum = l1->val + l2->val + carryFlag;
+
+        	addNode(head, createNode(&head, sum % 10));
+            carryFlag = sum / 10;
+            l1 = l1->next;
+            l2 = l2->next;
+    	}
+    }
+
+    if(l1 == NULL && l2 != NULL) {
+        while(l2 != NULL) {
+            sum = l2->val + carryFlag;
+
+            addNode(head, createNode(&head, sum % 10));
+            carryFlag = sum / 10;
+            l2 = l2->next;
+        }
+    }
+    
+    if(l2 == NULL && l1 != NULL) {
+        while(l1 != NULL) {
+            sum = l1->val + carryFlag;
+
+            addNode(head, createNode(&head, sum % 10));
+            carryFlag = sum / 10;
+            l1 = l1->next;   
+        }
+    }
+    if(l1 == NULL && l2 == NULL) {
+       	if(carryFlag == 1) {
+           	addNode(head, createNode(&head, carryFlag));
+       	}
+    }
+
+    return head;   
+}
+
+//Method-2
+void updateNode(struct ListNode* l1, struct ListNode* l2, struct ListNode** ptr) {
+    int carryFlag = 0;
+    struct ListNode* temp = NULL;
+
+    if(!l1 && !l2)
+        return;
+
+    while(l1 || l2 || carryFlag) {
+        int sum = carryFlag;
+	    if(l1 != NULL) {
+	        sum = sum + l1->val;
+            l1 = l1->next;
+    	}
+        if(l2 != NULL) {
+	        sum = sum + l2->val;
+            l2 = l2->next;
+    	}
+
+        temp = (struct ListNode*)malloc(sizeof(struct ListNode));
+        temp->next = NULL;
+        temp->val = sum % 10;
+
+        carryFlag = sum / 10;
+
+        *ptr = temp;
+        ptr = &((*ptr)->next);   
+    } 
+}
+
+struct ListNode* addTwoNumbers1(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode* head = NULL;
+
+    updateNode(l1, l2, &head);
+
+    return head;
+}
+
+int main()
+{
+    struct ListNode l4 = {3, NULL}, l6 = {6, NULL};
+    struct ListNode l3 = {2, &l4}, l5 = {5, &l6};
+    struct ListNode l1 = {1, &l3}, l2 = {4, &l5}, *ptr = NULL;
+   
+    ptr = addTwoNumbers1(&l1, &l2);
+    printList(ptr);
+
+    return 0;
+}
