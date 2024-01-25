9055b417b9d02360336f1619231fc56260c8bf99
Prabhav <pvkumar5749404@gmail.com>
2024-01-25 23:18:33 +0530

    Converting the decimal integer to roman number
    Signed-off-by: Prabhav <pvkumar5749404@gmail.com>

--- intToRoman.c
+++ intToRoman.c
@@ -2,87 +2,73 @@
 #include<stdlib.h>
 #include<string.h>
 
-char* intToRoman1(int num) {
+char* intToRoman(int num) {
     char *roman = (char*)malloc(15);
-    int i = 0, j = 14;
+    int i = 0;
     memset(roman, 0, 15);
 
     while(num != 0) {
-        if(num >= 900) {
-            if(num >= 1000) {
-                roman[i++] = 'M';
-                num = num - 1000;
-            }
-            else {
-                roman[j--] = 'M';
-                num = 1000 - num;
-            }
-        } else if(num >= 400) {
-            if(num >= 500) {
-                roman[i++] = 'D';
-                num = num - 500;
-            }
-            else {
-                roman[j--] = 'D';
-                num = 500 - num;
-            }
+        if(num >= 1000) {
+            roman[i++] = 'M';
+            num -= 1000;
+        } else if(num >= 900) {
+            roman[i++] = 'C';
+            roman[i++] = 'M';
+            num -= 900;
+        } else if(num >= 500) {
+            roman[i++] = 'D';
+            num -= 500;
+        } else if (num >= 400) {
+            roman[i++] = 'C';
+            roman[i++] = 'D';
+            num -= 400;
+        } else if(num >= 100) {
+            roman[i++] = 'C';
+            num -= 100;
         } else if(num >= 90) {
-            if(num >= 100) {
-                roman[i++] = 'C';
-                num = num - 100;
-            }
-            else {
-                roman[j--] = 'C';
-                num =  100 - num;
-            }
+            roman[i++] = 'X';
+            roman[i++] = 'C';
+            num -= 90;
+        } else if(num >= 50) {
+            roman[i++] = 'L';
+            num -= 50;
         } else if(num >= 40) {
-            if(num >= 50) {
-                roman[i++] = 'L';
-                num = num - 50;
-            }
-            else {
-                roman[j--] = 'L';
-                num = 50 - num;
-            }
+            roman[i++] = 'X';
+            roman[i++] = 'L';
+            num -= 40;
+        } else if(num >= 10) {
+            roman[i++] = 'X';
+            num -= 10;
         } else if(num >= 9) {
-            if(num >= 10) {
-                roman[i++] = 'X';
-                num = num - 10;
-            }
-            else {
-                roman[j--] = 'X';
-                num = 10 - num;
-            }
+            roman[i++] = 'I';
+            roman[i++] = 'X';
+            num -= 9;
+        } else if(num >= 5) {
+            roman[i++] = 'V';
+            num -= 5;
         } else if(num >= 4) {
-            if(num >= 5) {
-                roman[i++] = 'V';
-                num = num - 5;
-            }
-            else {
-                roman[j--] = 'V';
-                num = 5 - num;
-            }
-        }
-        else {
+            roman[i++] = 'I';
+            roman[i++] = 'V';
+            num -= 4;
+        } else {
             if(num == 3) {
                 roman[i++] = 'I';
                 roman[i++] = 'I';
                 roman[i++] = 'I';
-            }
-            else if(num == 2) {
+            } else if(num == 2) {
                 roman[i++] = 'I';
                 roman[i++] = 'I';
             }
             else {
                 roman[i++] = 'I';
             }
-            num = 0;
+            num -= num;
         }
     }
     return roman;
 }
 
-char* intToRoman(int num) {
+char* intToRoman1(int num) {
     int symbolVal[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 3, 2, 1};
     char* symbol[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "III", "II", "I"};
 
